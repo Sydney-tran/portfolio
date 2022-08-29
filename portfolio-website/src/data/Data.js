26713c59625275e32cf110d9fd2cf0c987888c2e
Sydney Tran <sydney.tran@live.com>
2022-08-28 23:56:59 -0400

    add cornell bus tracker video

--- portfolio-website/src/data/Data.js
+++ portfolio-website/src/data/Data.js
@@ -1,19 +1,23 @@
 // projects
 export const projectsList = [
   {
-    img: "https://raw.githubusercontent.com/Sydney-tran/portfolio/main/portfolio-website/src/assets/finowl.png", 
+    img: "https://raw.githubusercontent.com/Sydney-tran/portfolio/main/portfolio-website/src/assets/finowl.png",
     has_vid: false,
     vid: "",
     title: "FinOwl",
     company: "MILL5",
     company_url: "https://www.mill5.com/",
     year: "May. 2022 - Aug. 2022",
-    description: "an application that helps financiers and stock traders retrieve quality market information through chat bots in platforms such as Microsoft Teams",
+    description:
+      "an application that helps financiers and stock traders retrieve quality market information through chat bots in platforms such as Microsoft Teams",
     has_partner1: false,
     has_partner2: false,
-    contribution1: "built frontend and backend of the internal administration site for managing FinOwl users and products",
-    contribution2: "developed a REST API with .NET 6 using n-tier architecture with unit and integration tests",
-    contribution3: "implemented SPA using React with TypeScript, RTK Query from Redux Toolkit, and a Metronic Bootstrap template",
+    contribution1:
+      "built frontend and backend of the internal administration site for managing FinOwl users and products",
+    contribution2:
+      "developed a REST API with .NET 6 using n-tier architecture with unit and integration tests",
+    contribution3:
+      "implemented SPA using React with TypeScript, RTK Query from Redux Toolkit, and a Metronic Bootstrap template",
     tech_stack: "C#, .NET, React, TypeScript",
     has_github: false,
     github_url: "",
@@ -21,38 +25,46 @@ export const projectsList = [
     demo_url: "",
   },
   {
-    img: "https://raw.githubusercontent.com/Sydney-tran/portfolio/main/portfolio-website/src/assets/cornellbustracker.jpg", 
-    has_vid: false, 
-    vid: "",
+    img: "https://raw.githubusercontent.com/Sydney-tran/portfolio/main/portfolio-website/src/assets/cornellbustracker.jpg",
+    has_vid: true,
+    vid: "https://raw.githubusercontent.com/Sydney-tran/portfolio/main/portfolio-website/src/assets/cornellbustracker.mp4",
     title: "Raspberry Pi Cornell Bus Tracker",
     company: "Personal Project",
     company_url: "https://github.com/Sydney-tran",
     year: "Jul. 2022 – Aug. 2022",
-    description: "a Cornell bus tracker that notifies students when to leave their dorm room to catch the bus from any stop just by looking at the LED lights in their room",
+    description:
+      "a Cornell bus tracker that notifies students when to leave their dorm room to catch the bus from any stop just by looking at the LED lights in their room",
     has_partner1: false,
     has_partner2: false,
-    contribution1: "performed web scraping of Tompkins Consolidated Area Transit website to collect route data",
-    contribution2: "displayed real-time departure information through LED lights’ colors, patterns, and flashing",
-    contribution3: "implemented project using Python, Raspberry Pi, and LED light strips",
+    contribution1:
+      "performed web scraping of Tompkins Consolidated Area Transit website to collect route data",
+    contribution2:
+      "displayed real-time departure information through LED lights’ colors, patterns, and flashing",
+    contribution3:
+      "implemented project using Python, Raspberry Pi, and LED light strips",
     tech_stack: "Python",
     has_github: true,
-    github_url: "https://github.com/Sydney-tran/raspberrypi_cornell_bus_tracker",
+    github_url:
+      "https://github.com/Sydney-tran/raspberrypi_cornell_bus_tracker",
     has_demo: false,
-    demo_url: "", 
+    demo_url: "",
   },
   {
-    img: "https://raw.githubusercontent.com/Sydney-tran/portfolio/main/portfolio-website/src/assets/designatcornell.jpg", 
-    has_vid: false, 
+    img: "https://raw.githubusercontent.com/Sydney-tran/portfolio/main/portfolio-website/src/assets/designatcornell.jpg",
+    has_vid: false,
     vid: "",
     company: "Cornell DTI",
     company_url: "https://www.cornelldti.org/",
     title: "Design@Cornell ",
     year: "Nov. 2021 – Present",
-    description: "a website that serves as a centralized repository of resources and information for the design community at Cornell",
+    description:
+      "a website that serves as a centralized repository of resources and information for the design community at Cornell",
     has_partner1: false,
     has_partner2: false,
-    contribution1: "collaborated with a sub-team of 8 Cornell student developers, designers, and project managers",
-    contribution2: "implemented the student orgs, design community, articles, and faculty pages using React with TypeScript",
+    contribution1:
+      "collaborated with a sub-team of 8 Cornell student developers, designers, and project managers",
+    contribution2:
+      "implemented the student orgs, design community, articles, and faculty pages using React with TypeScript",
     contribution3: "prepared website for launch in May 2022!",
     tech_stack: "React, TypeScript, HTML, CSS",
     has_github: true,
@@ -61,21 +73,28 @@ export const projectsList = [
     demo_url: "http://design.cornell.edu/",
   },
   {
-    img: "https://raw.githubusercontent.com/Sydney-tran/portfolio/main/portfolio-website/src/assets/critterworld.jpg", 
+    img: "https://raw.githubusercontent.com/Sydney-tran/portfolio/main/portfolio-website/src/assets/critterworld.jpg",
     has_vid: false,
     vid: "",
     title: "Critter World",
     company: "Cornell CS 2112",
     company_url: "https://www.cs.cornell.edu/courses/cs2112/2021fa/",
     year: "Oct. 2021 – Dec. 2021",
-    description: "a Java program that simulates evolving artificial life for the Cornell CS 2112 class final project",
+    description:
+      "a Java program that simulates evolving artificial life for the Cornell CS 2112 class final project",
     has_partner1: true,
-    partner1: {name: "Cathryn Li", url: "https://www.linkedin.com/in/cathryn-li-383239209/"},
+    partner1: {
+      name: "Cathryn Li",
+      url: "https://www.linkedin.com/in/cathryn-li-383239209/",
+    },
     has_partner2: true,
-    partner2: {name: "Tori Zhang", url: ""},
-    contribution1: "built a parser that generates abstract syntax trees and tested using fault injection",
-    contribution2: "implemented Model-View-Controller design pattern to create an interpreter, simulator, and user interface",
-    contribution3: "improved a critter's \"smell\" by implementing Dijkstra’s shortest path algorithm",
+    partner2: { name: "Tori Zhang", url: "" },
+    contribution1:
+      "built a parser that generates abstract syntax trees and tested using fault injection",
+    contribution2:
+      "implemented Model-View-Controller design pattern to create an interpreter, simulator, and user interface",
+    contribution3:
+      'improved a critter\'s "smell" by implementing Dijkstra’s shortest path algorithm',
     tech_stack: "Java",
     has_github: false,
     github_url: "",
@@ -83,19 +102,25 @@ export const projectsList = [
     demo_url: "",
   },
   {
-    img: "https://raw.githubusercontent.com/Sydney-tran/portfolio/main/portfolio-website/src/assets/sunsetcalc.png", 
-    has_vid: true, 
-    vid: "https://raw.githubusercontent.com/Sydney-tran/portfolio/main/portfolio-website/src/assets/sunsetcalculator.mp4", 
+    img: "https://raw.githubusercontent.com/Sydney-tran/portfolio/main/portfolio-website/src/assets/sunsetcalc.png",
+    has_vid: true,
+    vid: "https://raw.githubusercontent.com/Sydney-tran/portfolio/main/portfolio-website/src/assets/sunsetcalculator.mp4",
     title: "Sunset Calculator",
     company: "Personal Project",
     company_url: "https://github.com/Sydney-tran",
     year: "Feb. 2021 – Jun. 2021",
-    description: "a website that calculates the quality of upcoming sunsets and sunrises in an inputted zip code",
+    description:
+      "a website that calculates the quality of upcoming sunsets and sunrises in an inputted zip code",
     has_partner1: true,
-    partner1: {name: "Emily Chen", url: "https://www.linkedin.com/in/emily-m-chen/"},
+    partner1: {
+      name: "Emily Chen",
+      url: "https://www.linkedin.com/in/emily-m-chen/",
+    },
     has_partner2: false,
-    contribution1: "converted an inputted zip code into longitude and latitude using Google Maps API",
-    contribution2: "collected and presented sunrise/sunset data based on location using Sunset API",
+    contribution1:
+      "converted an inputted zip code into longitude and latitude using Google Maps API",
+    contribution2:
+      "collected and presented sunrise/sunset data based on location using Sunset API",
     contribution3: "implemented using JavaScript, HTML, and CSS",
     tech_stack: "JavaScript, HTML, CSS",
     has_github: true,
@@ -104,19 +129,23 @@ export const projectsList = [
     demo_url: "",
   },
   {
-    img: "https://raw.githubusercontent.com/Sydney-tran/portfolio/main/portfolio-website/src/assets/cosine.jpg", 
+    img: "https://raw.githubusercontent.com/Sydney-tran/portfolio/main/portfolio-website/src/assets/cosine.jpg",
     has_vid: false,
     vid: "",
     title: "Cosine Demo",
     company: "Harvard DASlab",
     company_url: "http://daslab.seas.harvard.edu/",
     year: "May. 2020 – Sep. 2020",
-    description: "a web demo for the Cosine (Cloud-Cost Optimized NoSQL Storage Engine) research paper",
+    description:
+      "a web demo for the Cosine (Cloud-Cost Optimized NoSQL Storage Engine) research paper",
     has_partner1: false,
     has_partner2: false,
-    contribution1: "collaborated with professor Stratos Idreos and his team from the Data Systems Laboratory",
-    contribution2: "implemented the UI and algorithms for uploading uniform or skewed data to the demo using JavaScript",
-    contribution3: "debugged and resolved issues involving corner cases for designing the storage engine configurations",
+    contribution1:
+      "collaborated with professor Stratos Idreos and his team from the Data Systems Laboratory",
+    contribution2:
+      "implemented the UI and algorithms for uploading uniform or skewed data to the demo using JavaScript",
+    contribution3:
+      "debugged and resolved issues involving corner cases for designing the storage engine configurations",
     tech_stack: "JavaScript, HTML, CSS",
     has_github: true,
     github_url: "https://github.com/Sydney-tran/demosubmitter_cloud",
@@ -147,7 +176,7 @@ export const imagesList = [
     img: "https://raw.githubusercontent.com/Sydney-tran/portfolio/main/portfolio-website/src/assets/mango.jpg",
     name: "mango",
   },
-];  
+];
 
 // about skills
 export const skillsList = [
@@ -197,5 +226,5 @@ export const certificationsList = [
   {
     name: "Microsoft Azure Fundamentals",
     img: "https://images.credly.com/size/220x220/images/be8fcaeb-c769-4858-b567-ffaaa73ce8cf/image.png",
-  }
-]
+  },
+];
